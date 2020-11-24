b509d94625d3993b5e46d4870ce5fb7842c81823
Bruce Scharlau <b.scharlau@abdn.ac.uk>
2020-11-25 00:43:29 +0900

    fixed settings.py and readme

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'temp_stories',
+    'temp_stories.apps.TempStoriesConfig',
 ]
 
 MIDDLEWARE = [
