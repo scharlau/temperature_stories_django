4fec70bfa8e2ea5f030ba2385176fa83dc11d04e
Bruce Scharlau <b.scharlau@abdn.ac.uk>
2021-03-08 10:23:58 +0000

    added tests

--- temp_stories/tests.py
+++ temp_stories/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from django.test import Client, TestCase
+from django.urls import reverse
 
 # Create your tests here.
+class TemeratureTests(TestCase):
+
+    def test_temp_text(self):
+        client = Client()
+        response = client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Enter the temperature in fahrenheit")
+    
+    def test_story_text(self):
+        client = Client()
+        response = client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "which spurs him into conflict with")
+    
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'temp_stories/index.html')
+    
+    def test_temp_form(self):
+        client = Client()
+        response = client.post('/', {'temp': 32})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Your converted temperature of 32 in F is 0.0 in C")
