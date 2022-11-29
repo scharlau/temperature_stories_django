dc153bb44b58573ceeb93219b3400531e3e35154
Bruce Scharlau <b.scharlau@abdn.ac.uk>
2022-11-29 10:14:59 +0000

    added guessing game

--- temp_stories/urls.py
+++ temp_stories/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('guess', views.guess, name='guess'),
 ]
--- temp_stories/views.py
+++ temp_stories/views.py
@@ -1,3 +1,4 @@
+import random
 from faker import Faker
 from django.shortcuts import render
 
@@ -18,6 +19,17 @@ def convert(temp):
     converted_temp = (temp-32)*0.5556
     return converted_temp
 
+def compare(guess, number):
+    answer = ""
+    if guess < number:
+        answer = "guess is low"
+    elif guess > number:
+        answer = "guess is high"
+    else:
+        answer = "you guesses correctly!"
+    return answer
+
+
 def index(request):
     mystory = story()
     converted_t = None
@@ -26,4 +38,14 @@ def index(request):
         temp = int(request.POST.get('temp',''))
         converted_t = convert(temp)
     return render(request, 'temp_stories/index.html', {'story': mystory, 'converted_t': converted_t, 'temp': temp})
+
+def guess(request):
+    number = random.randint(1,10)
+    guess = None
+    answer = None
+    if request.method == "POST":
+        guess = int(request.POST.get('guess',''))
+        answer = compare(guess, original)
+    return render(request, 'temp_stories/guess.html', {'guess': guess, 'number': number, 'answer': answer})
+
 
